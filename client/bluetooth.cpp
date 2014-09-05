0120a04e479513f1a092f9fcf835a6f00902af6b
Roman Moravčík <roman.moravcik@gmail.com>
2014-09-05 13:56:53 +0200

    Added complete firmware interface.

--- client/bluetooth.cpp
+++ client/bluetooth.cpp
@@ -31,26 +31,235 @@ Bluetooth::~Bluetooth()
     delete m_address;
 }
 
-void Bluetooth::start(void)
+void Bluetooth::requestConnection(void)
 {
+    if (!m_socket) {
+        return;
+    }
+
+    qDebug() << "Connecting...";
     m_socket->connectToService(*m_address, 1);
 }
 
-void Bluetooth::stop(void)
+void Bluetooth::requestDisconnection(void)
 {
+    if (!m_socket) {
+        return;
+    }
+
+    qDebug() << "Disconnecting...";
     m_socket->disconnectFromService();
+}
+
+bool Bluetooth::isConnected(void)
+{
+    if (!m_socket) {
+        return false;
+    }
+
+    if (m_socket->state() == QBluetoothSocket::ConnectedState) {
+                return true;
+    }
+
+    return false;
+}
+
+void Bluetooth::setHelmet(HelmetState state)
+{
+    if (state == HelmetOpen)
+    {
+        sendData("HELMET OPEN\r\n");
+    } else
+    {
+        sendData("HELMET CLOSE\r\n");
+    }
+}
+
+void Bluetooth::getHelmet(void)
+{
+    sendData("HELMET\r\n");
+}
+
+void Bluetooth::setEyes(PowerState state)
+{
+    if (state == PowerOn)
+    {
+        sendData("EYES ON\r\n");
+    } else
+    {
+        sendData("EYES OFF\r\n");
+    }
+}
+
+void Bluetooth::getEyes(void)
+{
+    sendData("EYES\r\n");
+}
+
+void Bluetooth::setRepulsors(PowerState state)
+{
+    if (state == PowerOn)
+    {
+        sendData("REPULSORS ON\r\n");
+    } else
+    {
+        sendData("REPULSORS OFF\r\n");
+    }
+}
+
+void Bluetooth::getRepulsors(void)
+{
+    sendData("REPULSORS\r\n");
+}
+
+void Bluetooth::repulsorsBlast(Repulsor repulsor)
+{
+    if (repulsor == RepulsorLeft)
+    {
+        sendData("REPULSOR LEFT\r\n");
+    } else
+    {
+        sendData("REPULSOR RIGHT\r\n");
+    }
+}
+
+
+void Bluetooth::setUnibeam(PowerState state)
+{
+    if (state == PowerOn)
+    {
+        sendData("UNIBEAM ON\r\n");
+    } else
+    {
+        sendData("UNIBEAM OFF\r\n");
+    }
+}
+
+void Bluetooth::getUnibeam(void)
+{
+    sendData("UNIBEAM\r\n");
+}
+
+void Bluetooth::setIntensity(Device device, PowerIntensity intensity)
+{
+    if (device == DeviceEyes) {
+        sendData("INTENSITY EYES ");
+    } else if (device == DeviceRepulsors) {
+        sendData("INTENSITY REPULSORS ");
+    } else if (device == DeviceUnibeam) {
+        sendData("INTENSITY UNIBEAM ");
+    }
+
+    switch (intensity) {
+        case Intensity10:
+            sendData("0\r\n");
+            break;
+        case Intensity20:
+            sendData("1\r\n");
+            break;
+        case Intensity30:
+            sendData("2\r\n");
+            break;
+        case Intensity40:
+            sendData("3\r\n");
+            break;
+        case Intensity50:
+            sendData("4\r\n");
+            break;
+        case Intensity60:
+            sendData("5\r\n");
+            break;
+        case Intensity70:
+            sendData("6\r\n");
+            break;
+        case Intensity80:
+            sendData("7\r\n");
+            break;
+        case Intensity90:
+            sendData("8\r\n");
+            break;
+        case Intensity100:
+            sendData("9\r\n");
+            break;
+    }
+}
+
+void Bluetooth::getIntensity(Device device)
+{
+    if (device == DeviceEyes) {
+        sendData("INTENSITY EYES\r\n");
+    } else if (device == DeviceRepulsors) {
+        sendData("INTENSITY REPULSORS\r\n");
+    } else if (device == DeviceUnibeam) {
+        sendData("INTENSITY UNIBEAM\r\n");
+    }
+}
+
+void Bluetooth::setVolume(VolumeLevel level)
+{
+    switch (level) {
+        case Level0:
+            sendData("VOLUME 0\r\n");
+            break;
+        case Level1:
+            sendData("VOLUME 1\r\n");
+            break;
+        case Level2:
+            sendData("VOLUME 2\r\n");
+            break;
+        case Level3:
+            sendData("VOLUME 3\r\n");
+            break;
+        case Level4:
+            sendData("VOLUME 4\r\n");
+            break;
+        case Level5:
+            sendData("VOLUME 5\r\n");
+            break;
+        case Level6:
+            sendData("VOLUME 6\r\n");
+            break;
+        case Level7:
+            sendData("VOLUME 7\r\n");
+            break;
+    }
+}
+
+void Bluetooth::getVolume(void)
+{
+    sendData("VOLUME\r\n");
+}
+
+void Bluetooth::playQuote(void)
+{
+    sendData("FORTUNE\r\n");
+}
+
+void Bluetooth::getBattery(void)
+{
+    sendData("BATTERY\r\n");
+}
+
+void Bluetooth::getVersion(void)
+{
+    sendData("VERSION\r\n");
+}
+
+void Bluetooth::reboot(void)
+{
+    sendData("REBOOT\r\n");
 }
 
 void Bluetooth::onConnected(void)
 {
     qDebug() << "Connected...";
-    emit jarvisConnected();
+    emit connected();
 }
 
 void Bluetooth::onDisconnected(void)
 {
     qDebug() << "Disconnected...";
-    emit jarvisDisconnected();
+    emit disconnected();
 }
 
 void Bluetooth::onReadyRead(void)
@@ -61,4 +270,12 @@ void Bluetooth::onReadyRead(void)
 void Bluetooth::onError(QBluetoothSocket::SocketError error)
 {
     qDebug() << "Error: " << error << m_socket->errorString();
+}
+
+void Bluetooth::sendData(const QString &data)
+{
+    if (isConnected()) {
+        unsigned int len = data.length();
+        m_socket->write(data.toUtf8().constData(), len);
+    }
 }
--- client/bluetooth.h
+++ client/bluetooth.h
@@ -12,21 +12,103 @@ QTM_USE_NAMESPACE
 class Bluetooth : public QObject
 {
     Q_OBJECT
+    Q_ENUMS(HelmetState)
+    Q_ENUMS(PowerState)
+    Q_ENUMS(Repulsor)
+    Q_ENUMS(Device)
+    Q_ENUMS(PowerIntensity)
+    Q_ENUMS(VolumeLevel)
+
 public:
+    enum HelmetState {
+        HelmetOpen = 0,
+        HelmetClose
+    };
+
+    enum PowerState {
+        PowerOff = 0,
+        PowerOn
+    };
+
+    enum Repulsor {
+        RepulsorLeft = 0,
+        RepulsorRight
+    };
+
+    enum Device {
+        DeviceEyes = 0,
+        DeviceRepulsors,
+        DeviceUnibeam
+    };
+
+    enum PowerIntensity {
+        Intensity10 = 0,
+        Intensity20,
+        Intensity30,
+        Intensity40,
+        Intensity50,
+        Intensity60,
+        Intensity70,
+        Intensity80,
+        Intensity90,
+        Intensity100
+    };
+
+    enum VolumeLevel {
+        Level0 = 0,
+        Level1,
+        Level2,
+        Level3,
+        Level4,
+        Level5,
+        Level6,
+        Level7
+    };
+
     explicit Bluetooth(QObject *parent = 0);
     ~Bluetooth();
 
-    Q_INVOKABLE void start(void);
-    Q_INVOKABLE void stop(void);
+    Q_INVOKABLE void requestConnection(void);
+    Q_INVOKABLE void requestDisconnection(void);
+    Q_INVOKABLE bool isConnected(void);
+
+    Q_INVOKABLE void setHelmet(HelmetState state);
+    Q_INVOKABLE void getHelmet(void);
+
+    Q_INVOKABLE void setEyes(PowerState state);
+    Q_INVOKABLE void getEyes(void);
+
+    Q_INVOKABLE void setRepulsors(PowerState state);
+    Q_INVOKABLE void getRepulsors(void);
+
+    Q_INVOKABLE void repulsorsBlast(Repulsor repulsor);
+
+    Q_INVOKABLE void setUnibeam(PowerState state);
+    Q_INVOKABLE void getUnibeam(void);
+
+    Q_INVOKABLE void setIntensity(Device device, PowerIntensity intensity);
+    Q_INVOKABLE void getIntensity(Device device);
+
+    Q_INVOKABLE void setVolume(VolumeLevel level);
+    Q_INVOKABLE void getVolume(void);
+
+    Q_INVOKABLE void playQuote(void);
+
+    Q_INVOKABLE void getBattery(void);
+    Q_INVOKABLE void getVersion(void);
+
+    Q_INVOKABLE void reboot(void);
 
 private:
     QBluetoothLocalDevice *m_device;
     QBluetoothAddress *m_address;
     QBluetoothSocket *m_socket;
 
+    void sendData(const QString &data);
+
 signals:
-    void jarvisConnected(void);
-    void jarvisDisconnected(void);
+    void connected(void);
+    void disconnected(void);
 
 public slots:
     void onConnected(void);
